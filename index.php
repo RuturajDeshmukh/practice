199d53aba9ce3ec396111a6d95857977fa881708
RuturajDeshmukh <51050421+RuturajDeshmukh@users.noreply.github.com>
2019-05-26 21:02:18 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,25 +1,35 @@
-
-<html>
+<html lang="en_US">
 <head>
+	<title>StudentManagement</title>
 </head>
 <body>
-	<h1>example of image uploading</h1>
-	<form action="index.php" method="post" enctype="multipart/form-data">
-		<input type="file" name="img1" required="required"/>
-		<input type="submit" name="submit" value="upload"/>
-	</form>
+
+<h4 align="right"><a href="login.php">Admin login</a></h4>
+<h1 align="center">Welcome to Management System</h1>
+<form action="index.php" method="post">
+	<table>
+		<tr>
+			<td colspan="2">Student Information</td>
+		</tr>
+		<tr>
+			<td>Choose Standard</td>
+			<td>
+				<select name="std">
+					
+				<option value="1">1st</option>
+				<option value="2">2st</option>
+				<option value="3">3st</option>
+				<option value="4">4st</option>
+				<option value="5">5st</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>enter rollno</td>
+			<td><input type="text" name="rollno" required></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" name="submit" value="show"></td.
+		</tr>
 </body>
 </html>
-<?php
-if(isset($_POST['submit']))
-{
-	print_r($_FILES['img1']);
-	$imagename=$_FILES['img1']['name'];
-	$_tempname=$_FILES['img1']['tmp_name'];
-	$conn=mysqli_connect('localhost','root','','storeimg');
-	move_uploaded_file($_tempname,"images/$imagename");
-	$sq1="INSERT INTO `uploadimg`( `images`) VALUES ('$imagename')";
-	$run=mysqli_query($conn,$sq1);
-	echo "uploadsuccess";
-}
-?>
