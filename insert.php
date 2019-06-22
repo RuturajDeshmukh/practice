6ef661b1221169a177f640ee5096a665c7449525
Ruturaj Deshmukh <ruturaj2308@gmail.com>
2019-06-22 08:23:08 +0530

    revert body

--- insert.php
+++ insert.php
@@ -4,11 +4,11 @@ if (isset($_POST['submit']))
 {
 	
 $title=$_POST['title'];
-
+$body=$_POST['body'];
 $userid=$_POST['userid'];
 $con=mysqli_connect('localhost','root','','blogposts');
 
-$qry="INSERT INTO `blogs`( `user_id`, `title`) VALUES ('$userid','$title')";
+$qry="INSERT INTO `blogs`( `user_id`, `title`, `body`) VALUES ('$userid','$title','$body')";
 $s1=mysqli_query($con,$qry);
 if ($s1)
 {
